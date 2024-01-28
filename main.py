c3ae76c3a0d3216ec599211689c93c6706f108bc
Muhammad Hekal Maulana <maulanaahekal@gmail.com>
2024-01-28 15:27:46 +0800

    Feature Scaling Training set and test set

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 # Import Dataset
 dataset = pd.read_csv("Social_Network_Ads.csv")
@@ -11,4 +12,10 @@ y = dataset.iloc[:, -1].values
 # Splitting dataset
 X_train, X_test, y_train, y_test = train_test_split(X,y, random_state=0, test_size=0.25)
 
+# Feature Scaling
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+print(X_train)
 
